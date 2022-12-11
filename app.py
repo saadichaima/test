27d07317dfd1440eaa7c1a92f61b7eb44cef28e5
saadichaima11 <saadichaima11@gmail.com>
2022-12-11 01:29:27 +0100

    commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os 
 from random import randint
  
-for j in range(0,5):
+for j in range(0,1):
         d= str(j)+'days ago'
         with open('file.txt','a') as file :
             file.write(d)
         os.system('git add .')
-        os .system('git commit --date="2022-12-04" -m "commit"')
+        os .system('git commit --date="2022-12-11" -m "commit"')
 
 os.system('git push -u origin main')
